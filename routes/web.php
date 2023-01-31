37c5fbd9cdd2dc1f600975dc010984ec8573111d
vcemm24 <vcemm24@gmail.com>
2023-01-31 18:18:58 +0100

    add tag and search request...

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
@@ -14,22 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('listings',[
-        'listings'=>Listing::all()
-    ]);
-});
-
-Route::get('/listings/{id}', function ($id) {
-
-    $listing = Listing::find($id);
-    if($listing){
-        return view('listing',[
-            'listing' =>$listing
-        ]);
-    }
-    else{
-        abort('404');
-    }
-   
-});
+Route::get('/', [ListingController::class,'index']);
+Route::get('/listings/{listing}', [ListingController::class,'show']);
